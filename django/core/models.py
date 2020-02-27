95262209355154e147b17e6aeed5e6cec78bad90
semiotix <semiotix01@yahoo.co.jp>
2020-02-27 13:34:16 +0900

    commit

---
+++ django/core/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+
+class Album(models.Model):
+    POP = 0
+    JAZZ = 1
+    CLASSIC = 2
+    ROCK = 3
+    HIPHOP = 4
+    EDM = 5
+    ETC = 6
+    GENRE = ((POP, 'POP MUSIC'),
+             (JAZZ, 'JAZZ'),
+             (CLASSIC, 'CLASSIC MUSIC')
+             (ROCK, 'ROCK')
+             (HIPHOP, 'HIPHOP'),
+             (EDM, 'EDM'),
+             (ETC, 'OTHER'),
+             )
+    album_title = models.CharField(max_length=140)
+    year = models.PositiveIntegerField()
+    genre = models.IntegerField(choices=GENRE,default=POP)
+    spotify = models.URLField(blank=True)
+    def __str__(self):
+        return '{} {}'.format(self.album_title, self.year)
