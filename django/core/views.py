f11b92b9ff054a961caafe26f81b0564b4d18ecb
semiotix <semiotix01@yahoo.co.jp>
2020-03-05 12:08:57 +0900

    Signed-off-by: semiotix <semiotix01@yahoo.co.jp>

--- django/core/views.py
+++ django/core/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from core.models import Album
 
 # Create your views here.
+class AlbumList(ListView):
+    model = Album
