0b6ccb33381cccde0276de883dfbf98a91efc729
semiotix <semiotix01@yahoo.co.jp>
2020-03-13 08:03:40 +0900

    Signed-off-by: semiotix <semiotix01@yahoo.co.jp>

--- django/config/settings.py
+++ django/config/settings.py
@@ -23,9 +23,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=%b53wqtfbwawoj5qub^20_7x=#ct4+6h3vjlqfk30vfx3a(8='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+        'localhost',
+        '127.0.0.1'
+]
 
 
 # Application definition
